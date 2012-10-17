806465a70a0638b24d4a7b3dc486d6775b37d9ef
Michael Schleichardt <michael@schleichardt.net>
2012-10-17 22:55:51 +0200

    basic auth for heroku

---
+++ app/Global.scala
@@ -0,0 +1,24 @@
+//TODO this can be done in Java too
+
+import _root_.info.schleichardt.play2.basicauth._
+import play.api.{Logger, Play, GlobalSettings}
+import play.api.mvc.RequestHeader
+
+object Global extends GlobalSettings {
+  //-Dbasic.auth.enabled=true
+  lazy val basicAuthEnabled: Boolean = {
+    val enabled = Play.current.configuration.getBoolean("basic.auth.enabled").getOrElse(false)
+    Logger.info("basic auth enabled: " + enabled)
+    enabled
+  }
+
+  val credentialSource = new CredentialsFromConfCheck
+
+  override def onRouteRequest(request: RequestHeader) = {
+    if (basicAuthEnabled) {
+      requireBasicAuthentication(request, credentialSource) {super.onRouteRequest(request)}
+    } else {
+      super.onRouteRequest(request)
+    }
+  }
+}
