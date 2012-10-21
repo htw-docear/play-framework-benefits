b090149df8fa020a558742e096ead483c4c2d911
Michael Schleichardt <michael@schleichardt.net>
2012-10-21 18:43:35 +0200

    long polling without timeout

--- app/controllers/LongPolling.java
+++ app/controllers/LongPolling.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import play.Logger;
+import play.libs.F;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
 import sync.FileContent;
 import sync.LongPollingPropertyChangeListener;
 import views.html.LongPolling.index;
+
+import java.util.UUID;
 
 public class LongPolling extends Controller {
 
@@ -16,9 +20,26 @@ public class LongPolling extends Controller {
     public static Result feed() {
         Results.Chunks<String> chunks = new Results.StringChunks() {
             public void onReady(Results.Chunks.Out<String> out) {
-                FileContent.getInstance().addPropertyChangeListener(new LongPollingPropertyChangeListener(out));
+                final String id = UUID.randomUUID().toString();
+                Logger.debug("long polling: added client " + id);
+                final FileContent fileContent = FileContent.getInstance();
+                fileContent.addPropertyChangeListener(new LongPollingPropertyChangeListener(out, fileContent));
+                out.onDisconnected(new DisconnectCallback(id));
             }
         };
         return ok(chunks);
+    }
+
+    private static class DisconnectCallback implements F.Callback0 {
+        private final String id;
+
+        public DisconnectCallback(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public void invoke() throws Throwable {
+            Logger.debug("long polling: disconnected to client " + id);
+        }
     }
 }
--- app/sync/LongPollingPropertyChangeListener.java
+++ app/sync/LongPollingPropertyChangeListener.java
@@ -1,5 +1,7 @@
 package sync;
 
+import org.codehaus.jackson.node.ObjectNode;
+import play.libs.Json;
 import play.mvc.Results;
 
 import java.beans.PropertyChangeEvent;
@@ -7,14 +9,21 @@ import java.beans.PropertyChangeListener;
 
 public class LongPollingPropertyChangeListener implements PropertyChangeListener {
     private final Results.Chunks.Out<String> out;
+    private final FileContent fileContent;
 
-    public LongPollingPropertyChangeListener(Results.Chunks.Out<String> out) {
+    public LongPollingPropertyChangeListener(Results.Chunks.Out<String> out, FileContent fileContent) {
         this.out = out;
+        this.fileContent = fileContent;
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
-        out.write(propertyChangeEvent.getNewValue().toString());
+        final String content = propertyChangeEvent.getNewValue().toString();
+        ObjectNode result = Json.newObject();
+        result.put("syncedText", content);
+        result.put("refresh", true);
+        out.write(result.toString());
         out.close();
+        fileContent.removePropertyChangeListener(this);
     }
 }
