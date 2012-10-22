15ad062f5e1034c9a6df8735ba1236fec6b19d36
Michael Schleichardt <michael@schleichardt.net>
2012-10-22 15:47:21 +0200

    set server side timeout for fixing #1

--- app/controllers/LongPolling.java
+++ app/controllers/LongPolling.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import akka.util.Duration;
 import play.Logger;
-import play.libs.F;
+import play.Play;
+import play.libs.Akka;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
@@ -10,6 +12,7 @@ import sync.LongPollingPropertyChangeListener;
 import views.html.LongPolling.index;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class LongPolling extends Controller {
 
@@ -19,27 +22,24 @@ public class LongPolling extends Controller {
 
     public static Result feed() {
         Results.Chunks<String> chunks = new Results.StringChunks() {
-            public void onReady(Results.Chunks.Out<String> out) {
+            public void onReady(final Results.Chunks.Out<String> out) {
                 final String id = UUID.randomUUID().toString();
                 Logger.debug("long polling: added client " + id);
                 final FileContent fileContent = FileContent.getInstance();
                 fileContent.addPropertyChangeListener(new LongPollingPropertyChangeListener(out, fileContent));
-                out.onDisconnected(new DisconnectCallback(id));
+                //on disconnect does not work
+                //https://play.lighthouseapp.com/projects/82401/tickets/654
+                final Integer timeout = Play.application().configuration().getInt("longpolling.serverside.timeout");
+                Akka.system().scheduler().scheduleOnce(
+                        Duration.create(timeout, TimeUnit.MILLISECONDS),
+                        new Runnable() {
+                            public void run() {
+                                out.close();
+                            }
+                        }
+                );
             }
         };
         return ok(chunks);
-    }
-
-    private static class DisconnectCallback implements F.Callback0 {
-        private final String id;
-
-        public DisconnectCallback(String id) {
-            this.id = id;
-        }
-
-        @Override
-        public void invoke() throws Throwable {
-            Logger.debug("long polling: disconnected to client " + id);
-        }
     }
 }
